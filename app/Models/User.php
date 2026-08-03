7381015a4e071d3d0f056a066aa515bed0513059
Lester Lou Reyes <llreyes.589@gmail.com>
2026-08-03 21:02:30 +0800

    Add auditing functionality with configuration and migration setup
    - Implemented AuditableTrait in User model for auditing capabilities.
    - Added auditing configuration file for enabling and customizing audit settings.
    - Created migration for audits table to store audit records.
    - Updated composer dependencies to include auditing packages.

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,9 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, AuditableTrait;
 
     /**
      * The attributes that are mass assignable.
@@ -36,5 +35,3 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-    ];
-}
